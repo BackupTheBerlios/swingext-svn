e1c9799571815f63bdb1726024ba4cde61343b96
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2006-09-07 10:52:40 +0000

    Import

---
+++ trunk/core/src/net/sarcommand/swingextensions/filechooser/ExtensionFileFilter.java
@@ -0,0 +1,86 @@
+package net.sarcommand.swingextensions.filechooser;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+/**
+ * Provides a default implementation for FileFilter instances which will filter by looking at the file's
+ * extension. This is a mere conveniance class to keep you the efford from writing an anonymous
+ * implementation every time you require an extension-based filter.
+ *
+ * todo add demo code
+ */
+public class ExtensionFileFilter extends FileFilter {
+    private final String[] _extensions;
+    private final String _description;
+    private final boolean _acceptDirectories;
+
+    /**
+     * Create a new FileFilter instance which will accept a file if it's extension is among the given list of
+     * valid extensions. All directories will be accepted as well.
+     *
+     * @param extensions A single extension or a list of extensions to be accepted by this FileFilter.
+     * @param description The description which should be displayed in the FileChooser when using this filter.
+     */
+    public ExtensionFileFilter(final String description, final String... extensions) {
+        this(description, true, extensions);
+    }
+
+    /**
+     * Create a new FileFilter instance which will accept a file if it's extension is among the given list of
+     * valid extensions. If required you can have the filter deny all directories by setting the 'acceptDirectories'
+     * parameter accordingly.
+     *
+     * @param extensions A single extensions or a list of suffixes to be accepted by this FileFilter.
+     * @param description The description which should be displayed in the FileChooser when using this filter.
+     * @param acceptDirectories Whether directories should be accepted by this filter.
+     */
+    public ExtensionFileFilter(final String description, final boolean acceptDirectories, final String... extensions){
+        _extensions = extensions;
+        _description = description;
+        _acceptDirectories = acceptDirectories;
+    }
+
+    /**
+     * Whether the given file is accepted by this filter.
+     */
+    public boolean accept(File f) {
+        if (_acceptDirectories && f.isDirectory())
+            return true;
+        final String fileName = f.getName();
+        final int dotIndex = fileName.lastIndexOf('.');
+        if (dotIndex <= 0)
+            return false;
+
+        final String extension = fileName.substring(dotIndex, fileName.length());
+        for (final String ext:_extensions)
+            if (extension.equalsIgnoreCase(ext))
+                return true;
+        return false;
+    }
+
+    /**
+     * The description of this filter. For example: "JPG and GIF Images"
+     * @see javax.swing.filechooser.FileView#getName
+     */
+    public String getDescription() {
+        return _description;
+    }
+
+    /**
+     * Returns the file name extensions which will be accepted by this filter.
+     *
+     * @return One or more extensions which will be accepted by this filter.
+     */
+    public String[] getExtensions() {
+        return _extensions;
+    }
+
+    /**
+     * Returns whether this filter will accept directories.
+     * @return true if filter accepts directories, false otherwise.
+     */
+    public boolean doesAcceptDirectories(){
+        return _acceptDirectories;
+    }
+}
