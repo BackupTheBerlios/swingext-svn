9281d012a1f59a479d423ccc72445ea33618e121
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2006-09-07 10:55:36 +0000

    Added license

--- trunk/core/src/net/sarcommand/swingextensions/filechooser/ExtensionFileFilter.java
+++ trunk/core/src/net/sarcommand/swingextensions/filechooser/ExtensionFileFilter.java
@@ -9,6 +9,20 @@ import java.io.File;
  * implementation every time you require an extension-based filter.
  *
  * todo add demo code
+ *
+ * Copyright 2006 Torsten Heup
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ExtensionFileFilter extends FileFilter {
     private final String[] _extensions;
