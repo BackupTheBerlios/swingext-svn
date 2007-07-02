f454b779e8766cd19a3c1b6a722cfca14cee04ac
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2007-07-02 16:47:49 +0000

    Import

---
+++ trunk/core/src/net/sarcommand/swingextensions/actions/ActionHandler.java
@@ -0,0 +1,23 @@
+package net.sarcommand.swingextensions.actions;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * Interface which tags an object as a possible responder for triggered actions. Whenever an action managed by the
+ * ActionManager class is invoked, the ActionManager will follow the responder chain until a suitable responder has
+ * been found.
+ *
+ * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ */
+public interface ActionHandler {
+    /**
+     * Requests that the implementing object should handle the given action. Implementors need to return whether or
+     * not the action could actually be handled. If this method returns true, the ActionManager will stop searching
+     * for responders.
+     *
+     * @param identifier Identifier of the action being triggered.
+     * @param e          The according ActionEvent.
+     * @return Whether or not the implementor could handle the action.
+     */
+    public boolean handleAction(final Object identifier, final ActionEvent e);
+}
