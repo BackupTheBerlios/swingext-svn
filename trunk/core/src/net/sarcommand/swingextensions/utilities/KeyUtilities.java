96185d3c05a0f1ae4098a2d5798dc32924f57d8f
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2007-07-12 15:29:05 +0000

    Added methods addFocusTraversalKey and removeFocusTraversalKey

--- trunk/core/src/net/sarcommand/swingextensions/utilities/KeyUtilities.java
+++ trunk/core/src/net/sarcommand/swingextensions/utilities/KeyUtilities.java
@@ -1,6 +1,9 @@
 package net.sarcommand.swingextensions.utilities;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.InputEvent;
+import java.util.HashSet;
 
 /**
  * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
@@ -30,5 +33,25 @@ public class KeyUtilities {
         if ((modifiers & InputEvent.ALT_GRAPH_MASK) != 0)
             buffer.append("altGraph ");
         return buffer.toString();
+    }
+
+    public static void addFocusTraversalKey(final JComponent target, final int keyCode, final int modifiers, final int id) {
+        addFocusTraversalKey(target, KeyStroke.getAWTKeyStroke(keyCode, modifiers), id);
+    }
+
+    public static void addFocusTraversalKey(final JComponent target, final AWTKeyStroke stroke, final int id) {
+        final HashSet<AWTKeyStroke> strokes = new HashSet<AWTKeyStroke>(target.getFocusTraversalKeys(id));
+        strokes.add(stroke);
+        target.setFocusTraversalKeys(id, strokes);
+    }
+
+    public static void removeFocusTraversalKey(final JComponent target, final int keyCode, final int modifiers, final int id) {
+        removeFocusTraversalKey(target, KeyStroke.getAWTKeyStroke(keyCode, modifiers), id);
+    }
+
+    public static void removeFocusTraversalKey(final JComponent target, final AWTKeyStroke stroke, final int id) {
+        final HashSet<AWTKeyStroke> strokes = new HashSet<AWTKeyStroke>(target.getFocusTraversalKeys(id));
+        strokes.remove(stroke);
+        target.setFocusTraversalKeys(id, strokes);
     }
 }
