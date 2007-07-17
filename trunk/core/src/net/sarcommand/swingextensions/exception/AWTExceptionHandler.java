40cbc2b7be0d197939d83dadc97fe705ba9ac69a
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2007-07-17 13:59:42 +0000

    Cleanup

--- trunk/core/src/net/sarcommand/swingextensions/exception/AWTExceptionHandler.java
+++ trunk/core/src/net/sarcommand/swingextensions/exception/AWTExceptionHandler.java
@@ -1,16 +1,47 @@
 package net.sarcommand.swingextensions.exception;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
  */
 public abstract class AWTExceptionHandler {
-    public AWTExceptionHandler() {
+    protected static List<AWTExceptionHandler> __handlers =
+            Collections.synchronizedList(new ArrayList<AWTExceptionHandler>(2));
 
+    public static void addHandler(final AWTExceptionHandler handler) {
+        if (handler == null)
+            throw new IllegalArgumentException("Parameter 'handler' must not be null!");
+
+        __handlers.add(handler);
+        System.setProperty("sun.awt.exception.handler", ExceptionHandlerProxy.class.getName());
     }
 
-    public void install() {
-        System.setProperty("sun.awt.exception.handler", getClass().getName());
+    public static void removeHandler(final AWTExceptionHandler handler) {
+        if (handler == null)
+            throw new IllegalArgumentException("Parameter 'handler' must not be null!");
+
+        __handlers.remove(handler);
     }
 
-    public abstract void handle(final Throwable t);
+    public static void clearHandlers() {
+        __handlers.clear();
+    }
+
+    public static List<AWTExceptionHandler> getHandlers() {
+        return Collections.unmodifiableList(__handlers);
+    }
+
+    public abstract boolean handle(final Throwable t);
+
+    public static class ExceptionHandlerProxy {
+        public void handle(final Throwable t) {
+            for (AWTExceptionHandler handler : AWTExceptionHandler.getHandlers()) {
+                if (handler.handle(t))
+                    break;
+            }
+        }
+    }
 }
--- trunk/core/src/net/sarcommand/swingextensions/exception/DefaultAWTExceptionHandler.java
+++ trunk/core/src/net/sarcommand/swingextensions/exception/DefaultAWTExceptionHandler.java
@@ -7,22 +7,27 @@ public class DefaultAWTExceptionHandler extends AWTExceptionHandler {
     private static ExceptionDialog __dialog;
     private static String __message;
 
-
-    public static void setMessage(final String message) {
+    public void setMessage(final String message) {
         __message = message;
     }
 
-    public static void setTitle(String title) {
+    public void setTitle(String title) {
         getDialog().setTitle(title);
     }
 
-    public static ExceptionDialog getDialog() {
+    public ExceptionDialog getDialog() {
         if (__dialog == null)
             __dialog = new ExceptionDialog();
         return __dialog;
     }
 
-    public void handle(Throwable t) {
+    public boolean handle(Throwable t) {
         getDialog().display(null, __message, t);
+        return true;
+    }
+
+    public void install() {
+        AWTExceptionHandler.clearHandlers();
+        AWTExceptionHandler.addHandler(this);
     }
 }
