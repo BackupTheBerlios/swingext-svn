7fc3a57040f27fb8d32a74d0626cb5700df31a3f
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-02-04 21:31:46 +0000

    Added JavaDoc

--- trunk/core/src/net/sarcommand/swingextensions/utilities/ColorUtilities.java
+++ trunk/core/src/net/sarcommand/swingextensions/utilities/ColorUtilities.java
@@ -3,9 +3,30 @@ package net.sarcommand.swingextensions.utilities;
 import java.awt.*;
 
 /**
- * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ * This class offers a few utility methods useful when handling Colors.
+ * <p/>
+ * <hr/>
+ * Copyright 2006-2008 Torsten Heup
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ColorUtilities {
+    /**
+     * Returns a hex string representing the given color object.
+     *
+     * @param color color to be converted to hex string, non-null.
+     * @return a hex string representing the given color.
+     */
     public static String asHexString(final Color color) {
         if (color == null)
             throw new IllegalArgumentException("Parameter 'color' must not be null!");
@@ -24,6 +45,12 @@ public class ColorUtilities {
         return builder.toString();
     }
 
+    /**
+     * Parses a color instance from the given hex string.
+     *
+     * @param string string to parse color value from.
+     * @return a color instance parsed from the given hex string.
+     */
     public static Color fromHexString(final String string) {
         if (string == null)
             throw new IllegalArgumentException("Parameter 'string' must not be null!");
@@ -31,6 +58,13 @@ public class ColorUtilities {
         return new Color(Integer.parseInt(string, 16));
     }
 
+    /**
+     * Returns a lighter version of the given color.
+     *
+     * @param color     Base color to use.
+     * @param rgbOffset Offset by which the color should be lightened.
+     * @return a lighter version of the given color.
+     */
     public static Color lighter(final Color color, final int rgbOffset) {
         if (color == null)
             throw new IllegalArgumentException("Parameter 'color' must not be null!");
@@ -40,6 +74,13 @@ public class ColorUtilities {
         return new Color(red, green, blue, color.getAlpha());
     }
 
+    /**
+     * Returns a darker version of the given color.
+     *
+     * @param color     Base color to use.
+     * @param rgbOffset Offset by which the color should be darkened.
+     * @return a darker version of the given color.
+     */
     public static Color darker(final Color color, final int rgbOffset) {
         if (color == null)
             throw new IllegalArgumentException("Parameter 'color' must not be null!");
@@ -47,14 +88,5 @@ public class ColorUtilities {
         final int green = Math.max(color.getGreen() - rgbOffset, 0);
         final int blue = Math.max(color.getBlue() - rgbOffset, 0);
         return new Color(red, green, blue, color.getAlpha());
-    }
-
-    public static void main(String[] args) {
-        System.out.println(asHexString(Color.BLUE));
-        System.out.println(asHexString(Color.RED));
-        System.out.println(asHexString(Color.GREEN));
-        System.out.println(fromHexString(asHexString(Color.BLUE)).equals(Color.BLUE));
-        System.out.println(fromHexString(asHexString(Color.RED)).equals(Color.RED));
-        System.out.println(fromHexString(asHexString(Color.GREEN)).equals(Color.GREEN));
     }
 }
