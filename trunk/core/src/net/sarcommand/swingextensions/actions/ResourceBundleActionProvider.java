064b10aaaceb977ba7cd3b4c6b0cace0fa8d7101
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-03-19 23:26:07 +0000

    Added JavaDoc

--- trunk/core/src/net/sarcommand/swingextensions/actions/ResourceBundleActionProvider.java
+++ trunk/core/src/net/sarcommand/swingextensions/actions/ResourceBundleActionProvider.java
@@ -22,6 +22,21 @@ import java.util.ResourceBundle;
  * platform, which rises the issue of the right menu key. On windows, you usually trigger the copy action with ctrl+c,
  * while you use command+c on the mac. To circumvent this issue, you can use the token 'menu' in your property files to
  * define the correct accelerator. The action provider will replace it with the correct platform-specific key for you.
+ * <p/>
+ * <hr/>
+ * Copyright 2006-2008 Torsten Heup
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ResourceBundleActionProvider implements ActionProvider {
     /**
