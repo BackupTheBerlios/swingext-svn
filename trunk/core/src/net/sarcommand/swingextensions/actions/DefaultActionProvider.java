e613772ac75e629d0016478410df6e294a208934
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-03-19 21:54:42 +0000

    Added JavaDoc

--- trunk/core/src/net/sarcommand/swingextensions/actions/DefaultActionProvider.java
+++ trunk/core/src/net/sarcommand/swingextensions/actions/DefaultActionProvider.java
@@ -1,7 +1,23 @@
 package net.sarcommand.swingextensions.actions;
 
 /**
- * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ * Default action provider used by the ActionManager. This instance will return null for all action identifiers, it
+ * merely works as a placeholder.
+ * <p/>
+ * <hr/>
+ * Copyright 2006-2008 Torsten Heup
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class DefaultActionProvider implements ActionProvider {
     public ManagedAction createAction(final Object identifier) {
