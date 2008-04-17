d6a45745f4125dcaef7ae319dc1e1a9d2759ee75
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-04-17 10:00:00 +0000

    Renamed

---
+++ trunk/core/src/net/sarcommand/swingextensions/formatters/FileFormat.java
@@ -0,0 +1,25 @@
+package net.sarcommand.swingextensions.formatters;
+
+import java.io.File;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+
+/**
+ * A format implementation working on file instances. Conversion is done by simply using the file's absolute path.
+ */
+public class FileFormat extends Format {
+    public StringBuffer format(final Object obj, final StringBuffer toAppendTo, final FieldPosition pos) {
+        if (obj == null)
+            throw new IllegalArgumentException("Parameter 'value' must not be null!");
+        if (!(obj instanceof File))
+            throw new IllegalArgumentException("This instance can only handle values of type File");
+
+        toAppendTo.insert(pos.getBeginIndex(), ((File) obj).getAbsolutePath());
+        return toAppendTo;
+    }
+
+    public Object parseObject(final String source, final ParsePosition pos) {
+        return new File(source);
+    }
+}
