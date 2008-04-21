0ec8904022020d6597994d2791ffe564f1e22d39
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-04-21 21:11:44 +0000

    Import

--- trunk/core/src/net/sarcommand/swingextensions/treetable/DefaultTreeTableCellRenderer.java
+++ trunk/core/src/net/sarcommand/swingextensions/treetable/DefaultTreeTableCellRenderer.java
@@ -1,18 +1,28 @@
 package net.sarcommand.swingextensions.treetable;
 
-import net.sarcommand.swingextensions.table.AbstractCellRenderer;
+import net.sarcommand.swingextensions.table.CellRendererUtility;
 
+import javax.swing.*;
 import javax.swing.tree.TreePath;
 import java.awt.*;
 
 /**
+ * Default renderer used by JTreeTable instances.
  */
-public class DefaultTreeTableCellRenderer extends AbstractCellRenderer implements TreeTableCellRenderer {
+public class DefaultTreeTableCellRenderer implements TreeTableCellRenderer {
+    protected JLabel _renderer;
+    protected CellRendererUtility _cellRendererUtility;
+
+    public DefaultTreeTableCellRenderer() {
+        _cellRendererUtility = new CellRendererUtility();
+        _renderer = new JLabel();
+    }
+
     public Component getTreeTableCellRendererComponent(final JTreeTable table, final Object value,
                                                        final boolean isSelected, final boolean hasFocus,
                                                        final TreePath path, final int row, final int column) {
-        setText(value == null ? "" : value.toString());
-        adaptToTreeTable(this, table, isSelected, hasFocus, row, column);
-        return this;
+        _renderer.setText(value == null ? "" : value.toString());
+        _cellRendererUtility.adaptToTreeTable(_renderer, table, isSelected, hasFocus, row, column);
+        return _renderer;
     }
 }
