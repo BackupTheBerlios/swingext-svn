6475238e0bd213df679099725b8b98b32915fbfb
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-04-22 16:01:14 +0000

    Minor cleanup

--- trunk/core/src/net/sarcommand/swingextensions/selectiontree/DefaultSelectionTreeCellRenderer.java
+++ trunk/core/src/net/sarcommand/swingextensions/selectiontree/DefaultSelectionTreeCellRenderer.java
@@ -1,32 +1,30 @@
 package net.sarcommand.swingextensions.selectiontree;
 
-import net.sarcommand.swingextensions.internal.*;
+
+import net.sarcommand.swingextensions.internal.SwingExtResources;
+import static net.sarcommand.swingextensions.internal.SwingExtResources.getIconResource;
 
 import javax.swing.*;
-import javax.swing.tree.*;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
 
 /**
- * Default implementation for a JSelectionTree's renderer. This implementation will simply add suitable
- * checkbox icons in front of each node where applicable.
+ * Default implementation for a JSelectionTree's renderer. This implementation will simply add suitable checkbox icons
+ * in front of each node where applicable.
  * <p/>
- * If you want to change the icons being used, you can either use the getter/setter methods or globally define
- * your own icons by providing a custom ResourceBundle for the swingextensions package.
+ * If you want to change the icons being used, you can either use the getter/setter methods or globally define your own
+ * icons by providing a custom ResourceBundle for the swingextensions package.
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
+ * <hr/> Copyright 2006-2008 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
  * @see SwingExtResources *
  */
@@ -40,9 +38,9 @@ public class DefaultSelectionTreeCellRenderer extends DefaultTreeCellRenderer {
     private Icon _iconAllSelected;
 
     public DefaultSelectionTreeCellRenderer() {
-        _iconNoneSelected = SwingExtResources.getIconResource(ICON_NONE_SELECTED);
-        _iconSomeSelected = SwingExtResources.getIconResource(ICON_SOME_SELECTED);
-        _iconAllSelected = SwingExtResources.getIconResource(ICON_ALL_SELECTED);
+        _iconNoneSelected = getIconResource(ICON_NONE_SELECTED);
+        _iconSomeSelected = getIconResource(ICON_SOME_SELECTED);
+        _iconAllSelected = getIconResource(ICON_ALL_SELECTED);
     }
 
     public Icon getIconAllSelected() {
