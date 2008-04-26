513c0cc7baed96ff1b0da79c5605ea4cd9557562
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-04-26 17:58:04 +0000

    Added javadoc

--- trunk/core/src/net/sarcommand/swingextensions/event/EventSupport.java
+++ trunk/core/src/net/sarcommand/swingextensions/event/EventSupport.java
@@ -1,40 +1,37 @@
 package net.sarcommand.swingextensions.event;
 
-import java.lang.reflect.*;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.*;
 
 /**
  * This class provides a simple way of handling lists of event listeners, analogous to the PropertyChangeSupport class,
- * but for all kinds of listeners. You obtain an EventSupport by using the create(Class) method:
- * <code>
- * final EventSupport<ActionListener> actionListeners = EventSupport.create(ActionListener.class);
- * </code>
- * You can then (un-)register listeners using the according addListener(T) and removeListener(T) methods, T being the
- * type of listener supported by the EventSupport object obtain. If you want to propagate an event to all registered
- * listener instances, you can use the delegate() method. It will return a proxy of the correct class, which will
- * delegate all method calls to each of the listeners. For instance, an EventSupport created to hold ActionListeners
- * would be used like following:
- * <code>
- * final ActionEvent event = new ActionEvent(...);
- * actionListeners.delegate().actionPerformed(event);
- * </code>
- * By doing so, event propagation becomes a one-liner. Since the delegate() method returns a full implementation of the
- * listener interface, listeners with multiple methods are supported as well.
+ * but for all kinds of listeners. You obtain an EventSupport by using the create(Class) method: <code> final
+ * EventSupport<ActionListener> actionListeners = EventSupport.create(ActionListener.class); </code> You can then
+ * (un-)register listeners using the according addListener(T) and removeListener(T) methods, T being the type of
+ * listener supported by the EventSupport object obtain. If you want to propagate an event to all registered listener
+ * instances, you can use the delegate() method. It will return a proxy of the correct class, which will delegate all
+ * method calls to each of the listeners. For instance, an EventSupport created to hold ActionListeners would be used
+ * like following: <code> final ActionEvent event = new ActionEvent(...); actionListeners.delegate().actionPerformed(event);
+ * </code> By doing so, event propagation becomes a one-liner. Since the delegate() method returns a full implementation
+ * of the listener interface, listeners with multiple methods are supported as well.
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Be aware that there is another implementation available which uses weak references to store listeners. If you prefer
+ * this approach, you might want to use a <code>WeakEventSupport</code>.
+ *
+ * @see WeakEventSupport
+ *      <p/>
+ *      <hr/> Copyright 2006-2008 Torsten Heup
+ *      <p/>
+ *      Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ *      with the License. You may obtain a copy of the License at
+ *      <p/>
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      <p/>
+ *      Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *      on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *      for the specific language governing permissions and limitations under the License.
  */
 public class EventSupport<T extends EventListener> {
     /**
@@ -105,8 +102,8 @@ public class EventSupport<T extends EventListener> {
 
     /**
      * This method returns a proxy object which will mimic the listener class this instance has been created for. The
-     * returned value will appear to be an EventListener of the specified type and delegate all method calls to the
-     * list of registered listeners.
+     * returned value will appear to be an EventListener of the specified type and delegate all method calls to the list
+     * of registered listeners.
      *
      * @return a proxy object which will mimic the listener class this instance has been created for.
      */
