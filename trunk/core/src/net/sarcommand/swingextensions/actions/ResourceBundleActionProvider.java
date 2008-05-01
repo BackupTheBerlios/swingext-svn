fd7c1b7ab7d1fe4117210ed585fd36c61e4f4d6f
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-05-01 21:13:36 +0000

    adapted to refactoring

--- trunk/core/src/net/sarcommand/swingextensions/actions/ResourceBundleActionProvider.java
+++ trunk/core/src/net/sarcommand/swingextensions/actions/ResourceBundleActionProvider.java
@@ -10,33 +10,28 @@ import java.util.ResourceBundle;
 /**
  * An ActionProvider instance using a resource bundle to create Action instances. It will use the toString() method on
  * the Action's identifier and use it to locate the according properties. By default, it will look for the pattern
- * <code><actionIdentifier.toString()><pathKey><actionProperty></code>. The 'pathKey' element is a merely cosmetic token
- * you can define to make the bundle files more readable. The 'actionProperty' element is the property of the Action
- * instance you want to set. <code>myAction.Name</code> would set the 'name' property for an Action identified by the
- * string 'myAction', where '.' is the path key. Apart from the obvious string properties (Name, ShortDescription etc)
- * you can also set image properties (SmallIcon, SwingLargeIconKey). If you do, the property value will be referred to
- * the ImageCache, which attempts to load an icon with the given name. Futhermore, you can set acclerator keys which
- * will automatically be converted to an appropriate KeyStroke instance.
+ * <code><actionIdentifier.toString()>.<actionProperty></code>. The 'actionProperty' element is the property of the
+ * Action instance you want to set. <code>myAction.Name</code> would set the 'name' property for an Action identified by
+ * the string 'myAction'. Apart from the obvious string properties (Name, ShortDescription etc) you can also set image
+ * properties (SmallIcon, SwingLargeIconKey). If you do, the property value will be referred to the ImageCache, which
+ * attempts to load an icon with the given name. Futhermore, you can set acclerator keys which will automatically be
+ * converted to an appropriate KeyStroke instance.
  * <p/>
  * Side note: There is one special thing about accelerator keys: Since actions should behave consistently with the
  * platform, which rises the issue of the right menu key. On windows, you usually trigger the copy action with ctrl+c,
  * while you use command+c on the mac. To circumvent this issue, you can use the token 'menu' in your property files to
  * define the correct accelerator. The action provider will replace it with the correct platform-specific key for you.
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
+ * <hr/> Copyright 2006-2008 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class ResourceBundleActionProvider implements ActionProvider {
     /**
@@ -51,50 +46,22 @@ public class ResourceBundleActionProvider implements ActionProvider {
     protected ResourceBundle _resourceBundle;
 
     /**
-     * The path key being used, if any.
-     */
-    protected String _pathKey;
-
-    /**
-     * Creates a new action provider using the given bundle, without a path key.
+     * Creates a new action provider using the given bundle.
      *
      * @param bundleName The name of the ResourceBundle to use (will be passed to ResourceBundle.getBundle(String)
      */
     public ResourceBundleActionProvider(final String bundleName) {
-        this(ResourceBundle.getBundle(bundleName), "");
+        this(ResourceBundle.getBundle(bundleName));
     }
 
     /**
-     * Creates a new action provider using the given bundle and path key.
-     *
-     * @param bundleName The name of the ResourceBundle to use (will be passed to ResourceBundle.getBundle(String)
-     * @param pathKey    The token to use as path key
-     */
-    public ResourceBundleActionProvider(final String bundleName, final String pathKey) {
-        this(ResourceBundle.getBundle(bundleName), pathKey);
-    }
-
-    /**
-     * Creates a new action provider using the given bundle, without a path key.
+     * Creates a new action provider using the given bundle.
      *
      * @param bundle The resource bundle to use.
      */
     public ResourceBundleActionProvider(final ResourceBundle bundle) {
-        this(bundle, "");
-    }
-
-    /**
-     * Creates a new action provider using the given bundle and path key.
-     *
-     * @param bundle  The resource bundle to use.
-     * @param pathKey The token to use as path key
-     */
-    public ResourceBundleActionProvider(final ResourceBundle bundle, final String pathKey) {
         if (bundle == null)
             throw new IllegalArgumentException("Parameter 'bundle' must not be null!");
-        if (pathKey == null)
-            throw new IllegalArgumentException("Parameter '_pathKey' must not be null!");
-        _pathKey = pathKey;
         _resourceBundle = bundle;
     }
 
@@ -120,7 +87,7 @@ public class ResourceBundleActionProvider implements ActionProvider {
      * @param identifier The unique action identifier to use
      * @return a ManagedAction from the resource bundle
      */
-    public ManagedAction createAction(final Object identifier) {
+    public ManagedAction createManagedAction(final Object identifier) {
         final ManagedAction action = new ManagedAction(identifier);
         map(identifier, action);
 
@@ -128,14 +95,14 @@ public class ResourceBundleActionProvider implements ActionProvider {
     }
 
     /**
-     * This method will map all properties found in the resource bundle for the action identifier to the given
-     * action instance. Subclasses which provide their own action implementation may want to reuse this method.
+     * This method will map all properties found in the resource bundle for the action identifier to the given action
+     * instance. Subclasses which provide their own action implementation may want to reuse this method.
      *
      * @param identifier Unique action identifier used to look up properties in the resource bundle.
      * @param action     The action to which the looked up properties will be mapped.
      */
     public void map(Object identifier, Action action) {
-        final String prefix = identifier.toString() + _pathKey;
+        final String prefix = identifier.toString();
         final int length = prefix.length();
 
         for (String s : _resourceBundle.keySet()) {
