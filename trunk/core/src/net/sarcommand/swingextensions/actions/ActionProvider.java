c42ff79a246dd3e46fe0d1da44da736bbe1427f7
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-05-01 21:13:22 +0000

    Added method createReflectedAction, renamed createAction to createManagedAction

--- trunk/core/src/net/sarcommand/swingextensions/actions/ActionProvider.java
+++ trunk/core/src/net/sarcommand/swingextensions/actions/ActionProvider.java
@@ -4,28 +4,35 @@ package net.sarcommand.swingextensions.actions;
  * Interface for classes used to instanciate Actions. Instances of this class will mainly be used by the ActionManager,
  * but can be employed by themselves as well.
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
+ * <hr/> Copyright 2006-2008 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public interface ActionProvider {
     /**
-     * Creates an Action instance for the given identifier. How the action is being created has to be
-     * determined by the implementing class.
+     * Creates an ManagedAction instance for the given identifier. How the action is being created has to be determined
+     * by the implementing class.
      *
      * @param identifier Unique identifier for an Action
      * @return the Action corresponding to the specified identifier.
      */
-    public ManagedAction createAction(final Object identifier);
+    public ManagedAction createManagedAction(final Object identifier);
+
+    /**
+     * Used to obtain a ReflectedAction, using the given target object and method name.
+     *
+     * @param identifier The unique action identifier to use
+     * @param target     The target object on which the specified method will be invoked
+     * @param methodName The name of the method to invoke when the action is perfomed
+     * @return a ReflectedAction, using the given target object and method name.
+     * @see ReflectedAction
+     */
+    public ReflectedAction createReflectedAction(final Object identifier, final Object target, final String methodName);
 }
