16e633ccd488bacf128f50336f7e7730f0361a2b
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-05-17 08:59:43 +0000

    Added selection tree demo

--- trunk/demo/src/net/sarcommand/swingextensions/demo/SwingExtensionsDemo.java
+++ trunk/demo/src/net/sarcommand/swingextensions/demo/SwingExtensionsDemo.java
@@ -8,22 +8,19 @@ import java.awt.*;
  * <p/>
  * Copyright 2006 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class SwingExtensionsDemo {
     private static final Class[] DEMO_CLASSES = {
             JImagePanelDemo.class,
-//            SelectionTreeDemo.class,
+            SelectionTreeDemo.class,
             TypedInputFieldDemo.class,
             BlockingGlassPaneDemo.class,
             AccessoryManagerDemo.class,
@@ -33,7 +30,11 @@ public class SwingExtensionsDemo {
     private JTabbedPane _demoPanel;
 
     public SwingExtensionsDemo() {
-        initialize();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                initialize();
+            }
+        });
     }
 
     protected void initialize() {
