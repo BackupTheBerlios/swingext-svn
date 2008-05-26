0f9b3dcfce4280079eebce1f54255ddd0fb5d8b5
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-05-26 20:48:49 +0000

    Added license

--- trunk/core/src/net/sarcommand/swingextensions/applicationsupport/ApplicationParameter.java
+++ trunk/core/src/net/sarcommand/swingextensions/applicationsupport/ApplicationParameter.java
@@ -8,6 +8,17 @@ import java.net.URL;
  * This class represents an application runtime parameter. A parameter will have a name, a type, and a set of possible
  * keys. For instance, the "verbose" option might be triggered using the key "-v" or "--verbose". The application class
  * is reponsible for processing runtime arguments and assigning values to the correct instances of this class.
+ * <p/>
+ * <hr/> Copyright 2006-2008 Torsten Heup
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class ApplicationParameter {
     /**
