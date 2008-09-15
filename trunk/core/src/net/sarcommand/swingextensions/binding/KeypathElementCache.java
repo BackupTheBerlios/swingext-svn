e5c0159e631a1a5a0c65847e44ca236ebdfed7de
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2008-09-15 21:20:56 +0000

    Added property 'ignoreAccessControl'

--- trunk/core/src/net/sarcommand/swingextensions/binding/KeypathElementCache.java
+++ trunk/core/src/net/sarcommand/swingextensions/binding/KeypathElementCache.java
@@ -1,6 +1,6 @@
 package net.sarcommand.swingextensions.binding;
 
-import net.sarcommand.swingextensions.utilities.Pair;
+import net.sarcommand.swingextensions.utilities.Triple;
 
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
@@ -14,7 +14,7 @@ import java.util.HashMap;
  * <b>This is an internal class. You should never have to deal with it directly</b>
  */
 public class KeypathElementCache {
-    private static SoftReference<HashMap<Pair<Class, String>, KeypathElement>> __elementCache;
+    private static SoftReference<HashMap<Triple<Class, String, Boolean>, KeypathElement>> __elementCache;
 
     /**
      * Returns the cached KeypathElement for accessing the given property on the specified class. If no cached
@@ -24,19 +24,23 @@ public class KeypathElementCache {
      * @param property name of the property being accessed.
      * @return the cached KeypathElement for accessing the given property on the specified class.
      */
-    public static synchronized KeypathElement getElement(final Class clazz, final String property) {
-        HashMap<Pair<Class, String>, KeypathElement> cache = __elementCache == null ? null : __elementCache.get();
+    public static synchronized KeypathElement getElement(final Class clazz, final String property,
+                                                         final Boolean ignoreAccessControl) {
+        HashMap<Triple<Class, String, Boolean>, KeypathElement> cache =
+                __elementCache == null ? null : __elementCache.get();
 
         if (cache == null) {
-            cache = new HashMap<Pair<Class, String>, KeypathElement>(64);
-            __elementCache = new SoftReference<HashMap<Pair<Class, String>, KeypathElement>>(cache);
+            cache = new HashMap<Triple<Class, String, Boolean>, KeypathElement>(64);
+            __elementCache = new SoftReference<HashMap<Triple<Class, String, Boolean>, KeypathElement>>(cache);
         }
 
-        final Pair<Class, String> key = new Pair<Class, String>(clazz, property);
+        final Triple<Class, String, Boolean> key = new Triple<Class, String, Boolean>(clazz, property,
+                ignoreAccessControl);
+
         KeypathElement element = cache.get(key);
 
         if (element == null) {
-            element = new KeypathElement(clazz, property);
+            element = new KeypathElement(clazz, property, ignoreAccessControl);
             cache.put(key, element);
         }
 
