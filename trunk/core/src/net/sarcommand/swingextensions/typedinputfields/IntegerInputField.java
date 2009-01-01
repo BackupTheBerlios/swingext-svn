4f74f1aab7e5abd00213e2cb6d55afb5e020dedb
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-01 14:27:54 +0000

    Fixed: will return null if an integer value could not be parsed

--- trunk/core/src/net/sarcommand/swingextensions/typedinputfields/IntegerInputField.java
+++ trunk/core/src/net/sarcommand/swingextensions/typedinputfields/IntegerInputField.java
@@ -3,20 +3,16 @@ package net.sarcommand.swingextensions.typedinputfields;
 /**
  * A TypedInputField implementation which will only allow entering whole integer numbers.
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
+ * <hr/> Copyright 2006-2008 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class IntegerInputField extends AbstractTypedTextField<Integer> {
     public static final String PATTERN_INTEGER = "-?[0-9]+";
@@ -35,7 +31,12 @@ public class IntegerInputField extends AbstractTypedTextField<Integer> {
         final String text = getText();
         if (text.length() == 0)
             return null;
-        return Integer.parseInt(text);
+
+        try {
+            return Integer.valueOf(text);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
     /**
