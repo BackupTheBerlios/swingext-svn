d29017e8a321adf7b225aaa65aeef737e2fa1b6c
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-04 15:27:56 +0000

    Fixed: will remove test resources in testconnection()

--- trunk/core/src/net/sarcommand/swingextensions/label/LabelUtilities.java
+++ trunk/core/src/net/sarcommand/swingextensions/label/LabelUtilities.java
@@ -37,6 +37,7 @@ public class LabelUtilities {
                     textChanged();
                 }
             });
+            textChanged();
         }
 
         protected void textChanged() {
