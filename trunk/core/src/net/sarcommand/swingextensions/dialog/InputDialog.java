081214f3678693c3821b4b1625472a3266f91d00
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-04 18:45:14 +0000

    Fixed bug in changeOrCreateDirectory

---
+++ trunk/core/src/net/sarcommand/swingextensions/dialog/InputDialog.java
@@ -0,0 +1,19 @@
+package net.sarcommand.swingextensions.dialog;
+
+import javax.swing.*;
+
+/**
+ * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ */
+public class InputDialog {
+    public static final int DEFAULT_OPTION = JOptionPane.DEFAULT_OPTION;
+    public static final int OK_CANCEL_OPTION = JOptionPane.OK_CANCEL_OPTION;
+
+    public static String showTypedInputDialog(final String message, final String regExp, final int optionType) {
+        return null;
+
+    }
+
+    private InputDialog() {
+    }
+}
---
+++ trunk/core/src/net/sarcommand/swingextensions/dialog/TypedInputDialog.java
@@ -0,0 +1,31 @@
+package net.sarcommand.swingextensions.dialog;
+
+import javax.swing.*;
+
+/**
+ * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ */
+public class TypedInputDialog extends JDialog {
+    public TypedInputDialog() {
+        initialize();
+    }
+
+    protected void initialize() {
+        initComponents();
+        initLayout();
+        setupEventHandlers();
+    }
+
+    protected void initComponents() {
+
+    }
+
+    protected void initLayout() {
+
+    }
+
+    protected void setupEventHandlers() {
+
+    }
+
+}
