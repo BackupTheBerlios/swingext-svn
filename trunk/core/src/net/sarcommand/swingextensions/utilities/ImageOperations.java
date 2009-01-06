700ce5e078f671f2b17a91ad60a0190219e93023
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-06 23:44:55 +0000

    Added methods applyGlow(BufferedImage, Color, int) and applyGaussianBlur(BufferedImage, int)

--- trunk/core/src/net/sarcommand/swingextensions/utilities/ImageOperations.java
+++ trunk/core/src/net/sarcommand/swingextensions/utilities/ImageOperations.java
@@ -1,8 +1,7 @@
 package net.sarcommand.swingextensions.utilities;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.ConvolveOp;
-import java.awt.image.Kernel;
+import java.awt.*;
+import java.awt.image.*;
 
 /**
  * This class provides a set of methods aroung image and graphics manipulation. Most of those manipulation are mere
@@ -29,7 +28,75 @@ public class ImageOperations {
     public static BufferedImage applySimpleBoxBlur(final BufferedImage sourceImage) {
         final BufferedImage image = new BufferedImage(sourceImage.getWidth(), sourceImage.getHeight(),
                 sourceImage.getType());
-        image.createGraphics().drawImage(sourceImage, createSimpleBoxBlurFilter(), 0, 0);
+        final Graphics2D graphics2D = image.createGraphics();
+        graphics2D.drawImage(sourceImage, createSimpleBoxBlurFilter(), 0, 0);
+        graphics2D.dispose();
         return image;
+    }
+
+    public static BufferedImage applyGaussianBlur(final BufferedImage image, final int filterRadius) {
+        /* todo this can be handled more efficiently */
+        if (filterRadius < 1)
+            throw new IllegalArgumentException("Illegal filter radius: expected to be >= 1, was " + filterRadius);
+
+        float[] kernel = new float[2 * filterRadius + 1];
+
+        final float sigma = filterRadius / 3f;
+        final float alpha = 2f * sigma * sigma;
+        final float rootAlphaPI = (float) Math.sqrt(alpha * Math.PI);
+        float sum = 0;
+        for (int i = -0; i < kernel.length; i++) {
+            final int d = -((i - filterRadius) * (i - filterRadius));
+            kernel[i] = (float) (Math.exp(d / alpha) / rootAlphaPI);
+            sum += kernel[i];
+        }
+
+        for (int i = 0; i < kernel.length; i++)
+            kernel[i] /= sum;
+
+        final Kernel horizontalKernel = new Kernel(1, kernel.length, kernel);
+        final Kernel verticalKernel = new Kernel(kernel.length, 1, kernel);
+
+        final BufferedImage interim = new BufferedImage(image.getWidth(), image.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        final Graphics2D interimGraphics = interim.createGraphics();
+        interimGraphics.drawImage(image, new ConvolveOp(horizontalKernel, ConvolveOp.EDGE_NO_OP, null), 0, 0);
+        interimGraphics.dispose();
+
+        final BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        final Graphics2D resultGraphics = result.createGraphics();
+        resultGraphics.drawImage(interim, new ConvolveOp(verticalKernel, ConvolveOp.EDGE_NO_OP, null), 0, 0);
+        resultGraphics.dispose();
+
+        return result;
+    }
+
+    public static BufferedImage applyGlow(final BufferedImage image, final Color paint, final int radius) {
+        final BufferedImage blurred = applyGaussianBlur(image, radius);
+
+        final BufferedImage glow = new BufferedImage(image.getWidth() + 2 * radius, image.getHeight() + 2 * radius,
+                BufferedImage.TYPE_INT_ARGB);
+        final Graphics2D g2 = glow.createGraphics();
+
+        final int red = paint.getRed();
+        final int green = paint.getGreen();
+        final int blue = paint.getBlue();
+        final LookupTable table = new LookupTable(0, 4) {
+            @Override
+            public int[] lookupPixel(final int[] src, final int[] dest) {
+                dest[0] = red;
+                dest[1] = green;
+                dest[2] = blue;
+                dest[3] = src[3];
+                return dest;
+            }
+        };
+
+        g2.drawImage(blurred, new LookupOp(table, null), 0, 0);
+        g2.drawImage(image, 0, 0, null);
+        g2.dispose();
+
+        return glow;
     }
 }
