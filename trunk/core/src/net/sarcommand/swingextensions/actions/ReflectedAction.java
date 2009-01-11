b0e1ab9f922a78cdec069aebf4116aadbf42d691
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-11 17:49:44 +0000

    Updated javadoc

--- trunk/core/src/net/sarcommand/swingextensions/actions/ReflectedAction.java
+++ trunk/core/src/net/sarcommand/swingextensions/actions/ReflectedAction.java
@@ -6,27 +6,22 @@ import java.awt.event.ActionEvent;
 import java.lang.reflect.Method;
 
 /**
- * A ReflectedAction is an implementation of the ManagedAction concept which uses reflection to invoke a method when
- * the action is performed. It is created with a target object and the name of the method handling the action. By
- * convention, the method being notified has to meet one of the following two signatures:
- * <li>(Object, ActionEvent), in which case the action's identifier and the generated event will be passed as
- * parameters, or</li>
- * <li>() with no parameters, in which case the method will merely used as a notifier.</li>
+ * A ReflectedAction is an implementation of the ManagedAction concept which uses reflection to invoke a method when the
+ * action is performed. It is created with a target object and the name of the method handling the action. By
+ * convention, the method being notified has to meet one of the following two signatures: <li>(Object, ActionEvent), in
+ * which case the action's identifier and the generated event will be passed as parameters, or</li> <li>() with no
+ * parameters, in which case the method will merely used as a notifier.</li>
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
+ * <hr/> Copyright 2006-2008 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class ReflectedAction extends ManagedAction {
     /**
@@ -58,6 +53,10 @@ public class ReflectedAction extends ManagedAction {
         _methodName = methodName;
     }
 
+    /**
+     * @param e ActionEvent generated when triggering this action.
+     * @see javax.swing.Action#actionPerformed(java.awt.event.ActionEvent)
+     */
     public void actionPerformed(ActionEvent e) {
         Method method = SwingExtUtil.getMethod(_target, _methodName, Object.class, ActionEvent.class);
         if (method != null) {
