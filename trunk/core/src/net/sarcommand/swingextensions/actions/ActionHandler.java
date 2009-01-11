198002d4ebe3fffd762fe53c401588afea1fc5d0
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-11 17:36:13 +0000

    Updated license statement

--- trunk/core/src/net/sarcommand/swingextensions/actions/ActionHandler.java
+++ trunk/core/src/net/sarcommand/swingextensions/actions/ActionHandler.java
@@ -4,10 +4,19 @@ import java.awt.event.ActionEvent;
 
 /**
  * Interface which tags an object as a possible responder for triggered actions. Whenever an action managed by the
- * ActionManager class is invoked, the ActionManager will follow the responder chain until a suitable responder has
- * been found.
- *
- * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ * ActionManager class is invoked, the ActionManager will follow the responder chain until a suitable responder has been
+ * found.
+ * <p/>
+ * <hr/> Copyright 2006-2009 Torsten Heup
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public interface ActionHandler {
     /**
@@ -19,9 +28,9 @@ public interface ActionHandler {
     public static final String NEXT_HANDLER = ActionHandler.class.getName() + ".nextHandler";
 
     /**
-     * Requests that the implementing object should handle the given action. Implementors need to return whether or
-     * not the action could actually be handled. If this method returns true, the ActionManager will stop searching
-     * for responders.
+     * Requests that the implementing object should handle the given action. Implementors need to return whether or not
+     * the action could actually be handled. If this method returns true, the ActionManager will stop searching for
+     * responders.
      *
      * @param identifier Identifier of the action being triggered.
      * @param e          The according ActionEvent.
