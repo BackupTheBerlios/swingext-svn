6471764ecd1d8c746a6da1903e112ce2903513bf
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-01-27 23:47:54 +0000

    Reworked to use keypaths

--- trunk/core/src/net/sarcommand/swingextensions/treetable/ReflectingTreeTableModel.java
+++ trunk/core/src/net/sarcommand/swingextensions/treetable/ReflectingTreeTableModel.java
@@ -1,30 +1,26 @@
 package net.sarcommand.swingextensions.treetable;
 
+import net.sarcommand.swingextensions.binding.Keypath;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
-import java.lang.reflect.Method;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 /**
- * todo [heup] add docs
- * <hr/>
- * Copyright 2006-2009 Torsten Heup
+ * todo [heup] add docs <hr/> Copyright 2006-2009 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class ReflectingTreeTableModel extends AbstractTreeTableModel {
-    private LinkedHashMap<String, ReflectingTreeTableColumn> _columns;
+    private ArrayList<ReflectingTreeTableColumn> _columns;
 
     /**
      * Creates a tree in which any node can have children.
@@ -34,22 +30,20 @@ public class ReflectingTreeTableModel extends AbstractTreeTableModel {
      */
     public ReflectingTreeTableModel(final TreeNode root) {
         super(root);
-        _columns = new LinkedHashMap<String, ReflectingTreeTableColumn>(4);
+        _columns = new ArrayList<ReflectingTreeTableColumn>(8);
     }
 
     /**
-     * Creates a tree specifying whether any node can have children,
-     * or whether only certain nodes can have children.
+     * Creates a tree specifying whether any node can have children, or whether only certain nodes can have children.
      *
      * @param root               a TreeNode object that is the root of the tree
-     * @param asksAllowsChildren a boolean, false if any node can
-     *                           have children, true if each node is asked to see if
-     *                           it can have children
+     * @param asksAllowsChildren a boolean, false if any node can have children, true if each node is asked to see if it
+     *                           can have children
      * @see #asksAllowsChildren
      */
     public ReflectingTreeTableModel(final TreeNode root, final boolean asksAllowsChildren) {
         super(root, asksAllowsChildren);
-        _columns = new LinkedHashMap<String, ReflectingTreeTableColumn>(4);
+        _columns = new ArrayList<ReflectingTreeTableColumn>(8);
     }
 
 
@@ -60,61 +54,22 @@ public class ReflectingTreeTableModel extends AbstractTreeTableModel {
     public String getColumnLabel(final int columnIndex) {
         if (columnIndex >= _columns.size())
             throw new IllegalArgumentException("Invalid column index: " + columnIndex + " (this model only has " + _columns.size() + " columns)");
-        final String[] labels = _columns.keySet().toArray(new String[0]);
-        return labels[columnIndex];
+        return _columns.get(columnIndex).getLabel();
     }
 
     public Object getValueAt(final TreePath path, final int columnIndex) {
         if (columnIndex >= _columns.size())
             throw new IllegalArgumentException("Invalid column index: " + columnIndex + " (this model only has " + _columns.size() + " columns)");
 
-        final ReflectingTreeTableColumn col = _columns.get(getColumnLabel(columnIndex));
         Object value = path.getLastPathComponent();
         if (value instanceof DefaultMutableTreeNode)
             value = ((DefaultMutableTreeNode) value).getUserObject();
 
-        final Method getter = getGetterForClass(value.getClass(), col);
-        if (getter == null)
-            return null;
-        try {
-            return getter.invoke(value);
-        } catch (Exception e) {
-            throw new RuntimeException("Could not invoke method " + getter);
-        }
+        return _columns.get(columnIndex).getKeypath().get(value);
     }
 
-    public void registerColumn(final String label, final Class clazz, final String fieldName) {
-
-    }
-
-    public void registerColumn(final String label, final Class clazz, final Method getter) {
-        registerColumn(label, clazz, getter, null);
-    }
-
-    public void registerColumn(final String label, final Class clazz, final Method getter, final Method setter) {
-        if (!_columns.containsKey(label))
-            _columns.put(label, new ReflectingTreeTableColumn(label));
-        final ReflectingTreeTableColumn column = _columns.get(label);
-        column.setGetter(clazz, getter);
-        column.setSetter(clazz, setter);
-    }
-
-    protected Method getGetterForClass(Class clazz, final ReflectingTreeTableColumn column) {
-        Method m;
-
-        while (true) {
-            m = column.getGetter(clazz);
-            if (m != null)
-                break;
-            clazz = clazz.getSuperclass();
-            if (clazz == null)
-                break;
-        }
-
-        if (m != null)
-            return m;
-
-        return null;
+    public void addColumn(final String label, final String keypath) {
+        _columns.add(new ReflectingTreeTableColumn(label, new Keypath(keypath, true)));
     }
 }
 
