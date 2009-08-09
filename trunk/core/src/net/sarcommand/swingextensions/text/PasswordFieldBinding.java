68a58b3e8792bbc8094314bb09feb456642e0cb2
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-08-09 19:35:05 +0000

    Added support for UpdatePolicies

--- trunk/core/src/net/sarcommand/swingextensions/text/PasswordFieldBinding.java
+++ trunk/core/src/net/sarcommand/swingextensions/text/PasswordFieldBinding.java
@@ -8,7 +8,16 @@ import javax.swing.*;
 import static java.lang.String.format;
 
 /**
- * @author Torsten Heup <torsten.heup@fit.fraunhofer.de>
+ * <hr/> Copyright 2006-2009 Torsten Heup
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class PasswordFieldBinding extends AbstractTextComponentBinding<char[]> {
     private static final SwingExtLogger __log = SwingExtLogging.getLogger(PasswordFieldBinding.class);
@@ -19,8 +28,8 @@ public class PasswordFieldBinding extends AbstractTextComponentBinding<char[]> {
     public static final String CLIENT_PROPERTY = "swingext.PasswordFieldBindingClientProperty";
 
     public PasswordFieldBinding(final JPasswordField targetComponent, final Object targetBean, final String keypath,
-                                final boolean continuousUpdates, final boolean ignoreAccessControl) {
-        super(targetComponent, targetBean, keypath, continuousUpdates, ignoreAccessControl);
+                                final UpdatePolicy updatePolicy, final boolean ignoreAccessControl) {
+        super(targetComponent, targetBean, keypath, updatePolicy, ignoreAccessControl);
     }
 
     public PasswordFieldBinding(final JPasswordField targetComponent, final Object targetBean, final String keypath) {
