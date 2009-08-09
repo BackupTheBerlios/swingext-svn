f6e650737d0a2496ba52140de20cc6a36a24fd41
sarcan <sarcan@935e8d86-481c-0410-a422-b0df4306c4dd>
2009-08-09 16:39:54 +0000

    Fixed: Result of _tempFile#delete() was ignored

--- trunk/core/src/net/sarcommand/swingextensions/io/SecureFileOutputStream.java
+++ trunk/core/src/net/sarcommand/swingextensions/io/SecureFileOutputStream.java
@@ -6,26 +6,22 @@ import java.io.*;
  * Implements a FileOutputStream which will write to a temp file until the stream is closed and then transfer the
  * contents to the real destination file. You may find this class useful if you're performing non-trivial write
  * operations (such as the creation of large xml files) which might fail due to an exception. Rather than overwriting
- * the destination file and potentially destroying it, this implementation will preserve the destination file until
- * you 'commit' to the change by invoking close.
+ * the destination file and potentially destroying it, this implementation will preserve the destination file until you
+ * 'commit' to the change by invoking close.
  * <p/>
  * The created temp file will be deleted automatically when the application exits or the stream is closed. Furthermore,
  * you can invoke dispose when writing has failed to remove the temp file imediately.
  * <p/>
- * <hr/>
- * Copyright 2006-2008 Torsten Heup
+ * <hr/> Copyright 2006-2008 Torsten Heup
  * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class SecureFileOutputStream extends OutputStream {
     private final File _file;
@@ -62,18 +58,20 @@ public class SecureFileOutputStream extends OutputStream {
         in.getChannel().transferTo(0, _tempFile.length(), out.getChannel());
         out.close();
         in.close();
-        _tempFile.delete();
+        if (!_tempFile.delete())
+            _tempFile.deleteOnExit();
     }
 
     /**
-     * Disposes of this stream. You should invoke this method when writing to the output stream has failed to remove
-     * the created temp file. After invoking dispose, the stream should be considered.
+     * Disposes of this stream. You should invoke this method when writing to the output stream has failed to remove the
+     * created temp file. After invoking dispose, the stream should be considered.
      *
      * @throws IOException If the temp file could not be deleted.
      */
     public void dispose() throws IOException {
         _outStream.close();
-        _tempFile.delete();
+        if (!_tempFile.delete())
+            _tempFile.deleteOnExit();
     }
 
     public void write(final byte b[]) throws IOException {
